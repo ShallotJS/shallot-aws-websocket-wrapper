a5495c0a6a5b8a72291d691763e836965992c8d9
Bailey Tincher <btincher99@gmail.com>
2021-04-29 18:43:36 -0500

    Add websocket wrapper

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,76 @@
-export default true;
+import type { APIGatewayProxyEvent, Handler } from 'aws-lambda';
+import type { TShallotErrorHandlerOptions } from '@shallot/http-error-handler/dist/aws';
+
+import ShallotAWS from '@shallot/aws';
+import { ShallotAWSHttpErrorHandler } from '@shallot/http-error-handler';
+
+import ShallotAWSSocketJsonBodyParser, {
+  TShallotJSONBodyParserOptions,
+} from './json-body-parser';
+
+export type WebSocketRequestContext<DAuthorizer = undefined> =
+  APIGatewayProxyEvent['requestContext'] & {
+    connectionId: string;
+    authorizer: DAuthorizer;
+  };
+
+export type APIGatewayWebSocketEvent<DAuthorizer = undefined> = APIGatewayProxyEvent & {
+  requestContext: WebSocketRequestContext<DAuthorizer>;
+};
+
+type ParsedJSON = Record<string | number | symbol, unknown>;
+export type RequestDataBase = ParsedJSON | unknown;
+export type ResultDataBase = ParsedJSON | Array<ParsedJSON> | unknown;
+
+export type ShallotRawHandler<TEvent extends TShallotSocketEvent = TShallotSocketEvent> =
+  Handler<TEvent, void>;
+
+export type TShallotSocketEvent<
+  TQueryStringParameters extends RequestDataBase = unknown,
+  TPathParameters extends RequestDataBase = unknown,
+  THeaders extends RequestDataBase = unknown,
+  TBody extends RequestDataBase = unknown
+> = Omit<
+  Omit<
+    Omit<Omit<APIGatewayWebSocketEvent, 'body'>, 'queryStringParameters'>,
+    'pathParameters'
+  >,
+  'headers'
+> & {
+  queryStringParameters?: TQueryStringParameters;
+  pathParameters?: TPathParameters;
+  headers?: THeaders;
+  body?: TBody;
+};
+
+type TShallotSocketHandler = (
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  handler: ShallotRawHandler<any>,
+  successStatusCode?: number,
+  middlewareOpts?: {
+    HttpErrorHandlerOpts?: TShallotErrorHandlerOptions;
+    HttpJsonBodyParserOpts?: TShallotJSONBodyParserOptions;
+  }
+) => ShallotRawHandler<TShallotSocketEvent>;
+
+const ShallotSocketWrapper: TShallotSocketHandler = (
+  handler,
+  successStatusCode = 200,
+  middlewareOpts = {}
+) => {
+  const wrappedResponseHandler: Handler = async (...args) => {
+    const res = await handler(...args);
+    return {
+      statusCode: successStatusCode,
+      body: JSON.stringify(res),
+    };
+  };
+
+  const wrapper = ShallotAWS(wrappedResponseHandler)
+    .use(ShallotAWSSocketJsonBodyParser(middlewareOpts.HttpJsonBodyParserOpts))
+    .use(ShallotAWSHttpErrorHandler(middlewareOpts.HttpErrorHandlerOpts));
+
+  return wrapper;
+};
+
+export default ShallotSocketWrapper;
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,9 +1,33 @@
-import { test, describe, expect } from '@jest/globals';
+import type { Context } from 'aws-lambda';
 
-import Sample from '../src';
+import { test, describe, jest } from '@jest/globals';
 
-describe('', () => {
-  test('', () => {
-    expect(Sample).toBeTruthy();
+import ShallotAWSSocketWrapper from '../src';
+import type { ShallotRawHandler, TShallotSocketEvent } from '../src';
+
+describe('WebSocket Wrapper', () => {
+  const mockContext: Context = {
+    callbackWaitsForEmptyEventLoop: false,
+    functionName: '',
+    functionVersion: '',
+    invokedFunctionArn: '',
+    memoryLimitInMB: '',
+    awsRequestId: '',
+    logGroupName: '',
+    logStreamName: '',
+    getRemainingTimeInMillis: () => 0,
+    done: () => undefined,
+    fail: () => undefined,
+    succeed: () => undefined,
+  };
+
+  const mockHandler: ShallotRawHandler<TShallotSocketEvent<{ test: string }>> =
+    async () => undefined;
+
+  test('Smoke test CORS default usage', async () => {
+    const wrappedHandler = ShallotAWSSocketWrapper(mockHandler);
+
+    const mockEvent: TShallotSocketEvent = {} as unknown as TShallotSocketEvent;
+    await wrappedHandler(mockEvent, mockContext, jest.fn());
   });
 });
